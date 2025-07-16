f517bb0af5227b040bb53b082a12c2ed708a201e
tusharguptattn <tushar.gupta@tothenew.com>
2025-07-16 11:12:15 +0530

    added java 9,10,11,12 features

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.example {
+    requires java.base;
+    exports org.example;
+    exports org.example.Entity;
+    exports org.example.GenerateData;
+    exports org.example.Service;
+    exports org.example.Utility;
+}
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,15 @@ import org.example.GenerateData.DataGenerator;
 import org.example.Service.PayrollService;
 import org.example.Utility.EmployeeUtil;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Employee> employees = DataGenerator.generateEmployees(20);
+        var employees = DataGenerator.generateEmployees(20);
 
         System.out.println("All Employees:");
         employees.stream().map(i->i.getName()).forEach(System.out::println);
@@ -18,15 +22,66 @@ public class Main {
         System.out.println("\nActive Employees:");
         activeEmployees.stream().map(i->i.getName()).forEach(System.out::println);
 
-        double avgSalary = EmployeeUtil.calculateAverageSalary(activeEmployees);
+        var avgSalary = EmployeeUtil.calculateAverageSalary(activeEmployees);
         System.out.println("\nAverage Salary: " + avgSalary);
 
-        List<String> sortedNames = EmployeeUtil.getEmployeeNamesSorted(activeEmployees);
+        var sortedNames = EmployeeUtil.getEmployeeNamesSorted(activeEmployees);
         System.out.println("\nSorted Employee Names:");
         sortedNames.forEach(System.out::println);
 
         System.out.println("\nProcessing payrolls concurrently...");
-        PayrollService service = new PayrollService();
+        var service = new PayrollService();
         service.processPayrolls(activeEmployees);
-        }
+
+
+    /// //// Java 11and 12 Features
+//        String str = "Line1\nLine2\nLine3";
+//        str.lines().filter(s -> s.contains("1")).forEach(System.out::println);
+//        str.lines().forEach(System.out::println);
+//        System.out.println("    heloo    ".strip());
+//        System.out.println("    heloo    ".stripLeading());
+//        System.out.println("    heloo    ".stripTrailing());
+
+//        System.out.println("Hello, World!".repeat(3));
+//        System.out.println("  ".isBlank());
+
+
+        /// ///// Java 9 Features
+
+//        List<String> list = List.of("A", "B", "C");
+//        Set<Integer> set = Set.of(1, 2, 3);
+//        Map<String, Integer> map = Map.of("a", 1, "b", 2);
+
+
+        // takeWhile example
+//        List<Integer> list = List.of(1, 2, 3, 4, 5, 0, 6);
+//        list.stream()
+//                .takeWhile(n -> n > 0)
+//                .forEach(System.out::println); // Output: 1 2 3 4 5
+
+
+        // dropWhile example
+//        list.stream()
+//                .dropWhile(n -> n < 4)
+//                .forEach(System.out::println); // Output: 4 5 0 6
+
+// ofNullable example
+//        Stream<String> stream = Stream.ofNullable(null); // returns empty stream
+
+
+        /// /// java 10 Features
+
+        // var keyword example
+        // Note: 'var' can only be used for local variables, not class fields or method parameters.
+
+//        var name = "Sheru";     // inferred as String
+//        var count = 10;         // inferred as int
+//        var list = List.of("a","b");// inferred as ArrayList<String>
+//        System.out.println("Name: " + name);
+//        System.out.println("Count: " + count);
+//        System.out.println("List: " + list);
+
+
+
+    }
     }
